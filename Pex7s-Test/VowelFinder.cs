9a531eaf4d4b60bed6857c423ace7bb4525e8641
Lyle Hart III <lyle.hart@cellularsales.com>
2016-05-19 13:19:17 -0400

    added a CodeReview class and new tests

---
+++ Pex7s-Test/VowelFinder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pex7s;
+
+
+namespace Pex7s_Test
+{
+    /// <summary>
+    /// Summary description for VowelFinder
+    /// </summary>
+    [TestClass]
+    public class VowelFinder
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string testString = "blueberry";
+            Assert.IsTrue(RandomCodeReviewMethods.ContainsVowels(testString));   
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string testString = "BLUEBERRY";
+            Assert.IsTrue(RandomCodeReviewMethods.ContainsVowels(testString));
+        }
+    }
+}
---
+++ Pex7s/RandomCodeReviewMethods.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pex7s
+{
+    public static class RandomCodeReviewMethods
+    {
+        public static bool ContainsVowels(string input)
+        {
+            Console.WriteLine("We will check if your string contains any vowels or not.");
+            Console.Write("Enter a word: ");
+            //var vowelsInWord = Console.ReadLine().ToLower().Intersect("aeiou".ToCharArray());
+            var vowelsInWord = input.ToLower().Intersect("aeiou".ToCharArray());
+            if (!vowelsInWord.Any())
+            {
+                //Console.WriteLine("There are no vowels in this word, how do you pronounce that?");
+                return false;
+            }
+            else
+            {
+                return true;
+                //foreach (var vowel in vowelsInWord)
+                //{
+                //    Console.WriteLine($"Your word contains the vowel '{vowel}'");
+                //}
+            }
+        }
+    }
+}
